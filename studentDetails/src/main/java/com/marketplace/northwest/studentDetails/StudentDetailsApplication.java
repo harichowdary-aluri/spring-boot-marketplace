3b7f0a1e3e248ec653cf872fc7e0aa30a08e5340
Kiran Kumar Gundapuneni <98048115+kirangundapuneni@users.noreply.github.com>
2022-09-02 19:36:02 -0500

    Add files via upload

---
+++ studentDetails/src/main/java/com/marketplace/northwest/studentDetails/StudentDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.northwest.studentDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDetailsApplication.class, args);
+	}
+
+}
---
+++ studentDetails/src/test/java/com/marketplace/northwest/studentDetails/StudentDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.northwest.studentDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
