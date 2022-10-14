d0834feab99093131b46d4629e95321780f6600f
Sajida <sajida.mohammad2093@gmail.com>
2022-10-14 18:25:56 -0500

    Added the Header for the application

---
+++ Vue App/src/components/APPHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header" style="overflow-x: hidden">
+      <h1 class="navbarHeader">MARKETPLACE</h1>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: "APPHeader",
+    methods: {
+    },
+  };
+  </script>
+  
+  <style scoped>
+ 
+  .navbarHeader {
+    background-color: #1f5a7c;
+    font-weight: bold;
+    color: whitesmoke;
+    text-align: center;
+    height: 60px;
+    
+  }
+  .head{
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    
+  }
+  </style>
