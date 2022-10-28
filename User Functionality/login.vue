8fd6f966323797ea299de8a4ae2cc3fdc78517d8
ushareddypati <98170798+ushareddypati@users.noreply.github.com>
2022-10-28 18:17:32 -0500

    added login page in veu

---
+++ User Functionality/login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <form @submit.prevent="login">
+      <input v-model="username" placeholder="username">User Name
+      <input v-model="password" placeholder="password" type="password">Password
+      <input type="submit" value="log in">
+    </form>
+  </div>
+</template>
