458bb8a209d6a439ed1ff8477b8bdd7fae00bcb3
S547051 <aluriharikiran01@gmail.com>
2022-10-28 20:27:40 -0500

    Add files via upload

---
+++ User Functionality/HouseHoldRepository.java
@@ -0,0 +1,7 @@
+package com.productregistration.Repository;
+
+import com.productregistration.Entity.HouseHoldUtility;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseHoldRepository extends JpaRepository<HouseHoldUtility,Long>{
+}
