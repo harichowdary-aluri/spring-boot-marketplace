57ac2bd7d76c152bbbc1878b4c56f2fe44417594
Sajida <sajida.mohammad2093@gmail.com>
2022-11-24 19:08:40 -0600

    Miscellenous component added

---
+++ Vue App/src/components/Others.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <br>
+        <form class="offset-lg-3 col-md-6">
+            <br>
+            <div class="form-group row">
+                <label for="productName" class="col-sm-5 col-form-label">Product Name</label>
+                <div class="col-sm-5">
+                    <input type="text" class="form-control" v-model="Others.productName" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="noOFDaysUsed" class="col-sm-5 ">No Of Days Used</label>
+                <div class="col-sm-5">
+                    <input type="text" class="form-control" v-model="Others.daysUsed" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="qtnAvailable" class="col-sm-5">Qty Available</label>
+                <div class="col-sm-5">
+                    <input type="number" value="1" v-model="Others.qtyAvailable">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="price" class="col-sm-5 ">Price</label>
+                <div class="col-sm-5">
+                    <input type="text" class="form-control" v-model="Others.price" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="description" class="col-sm-5 ">Product Description</label>
+                <div class="col-sm-5">
+                    <input type="text" class="form-control" v-model="Others.description" />
+                </div>
+            </div>
+            <br>
+            <div class="form-group row">
+                <div class="col-sm-5">
+                    <input type="file" id="myFile" name="filename" multiple @change="onFileSelected">
+                </div>
+            </div>
+        </form>
+        <div style="text-align:center">
+            <button type="submit" v-on:click="RegisterProduct" class="btn btn-primary b1">SUBMIT</button>
+        </div>
+    </div>
+
+</template>
+<script>
+import axios from "axios";
+
+
+export default ({
+    name: "Otthers",
+
+    data() {
+        return {
+            Others: {
+                productName: "",
+                price:"",
+                daysUsed: "",
+                qtyAvailable: "",
+                description: "",
+                image: ""
+            }
+        };
+    },
+    methods: {
+
+        onFileSelected(event) {
+            this.Others.image = event.target.files[0];
+        },
+
+        RegisterProduct() {
+            axios.post
+        }
+
+    },
+});
+</script>
+<style>
+
+</style>
