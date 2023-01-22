a7beea2f82780514fce483239179b9338924942d
Kiran Kumar Gundapuneni <98048115+kirangundapuneni@users.noreply.github.com>
2023-01-22 13:22:52 -0600

    Updated Student Registration with UI Interaction

--- studentDetails/src/main/java/com/marketplace/northwest/controller/StudentController.java
+++ studentDetails/src/main/java/com/marketplace/northwest/controller/StudentController.java
@@ -1,3 +1,9 @@
+/**
+ * @Author: Your name
+ * @Date:   2022-09-30 09:36:13
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2023-01-22 13:21:51
+ */
 package com.marketplace.northwest.controller;
 
 import org.apache.catalina.connector.Response;
@@ -8,14 +14,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping("/Student")
-//public class StudentController {
-//    Repositories repo;
-//    @PostMapping("/Register")
-//    public Response SaveStudent(@RequestBody Student Student)
-//    {
-//    	return Repository.save(student)
-//    }
-//    
-//}
+@RestController
+public class StudentController {
+    @Autowired
+    StudentService studentservice;
+
+    @PostMapping("/register")
+    @CrossOrigin(origins = "http://localhost:8080")
+    public String registerUser(@RequestBody Student newStudent) {
+        System.out.println("Student object is " + newStudent);
+        studentservice.save(newStudent);
+        return "Student Registered";
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+}
