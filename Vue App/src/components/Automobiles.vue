60aff7eb75a8dc1223c537e36eda4017bdc079a7
ushareddypati <98170798+ushareddypati@users.noreply.github.com>
2023-01-22 19:37:36 -0600

    Added subcategories to Automobiles page

--- Vue App/src/components/Automobiles.vue
+++ Vue App/src/components/Automobiles.vue
@@ -10,6 +10,7 @@
                         <option class="form-control">Trucks</option>
                         <option class="form-control">2-Wheelers</option>
                         <option class="form-control">Others</option>
+
                     </select>
                 </div>
             </div>
@@ -126,6 +127,3 @@ export default ({
     },
 });
 </script>
-<style>
-
-</style>
