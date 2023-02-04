37bb88dbbf47ca84489da378ca5749d623c3d3f8
ushareddypati <98170798+ushareddypati@users.noreply.github.com>
2023-02-03 21:23:47 -0600

    Create AWSS3 configuration file.
    Get AmazonS3 client and return the s3Client object.

---
+++ Student Registration backend/FirstSample_1/src/main/java/com/marketplace/Helpers/config/AWSS3Config.java
@@ -0,0 +1,33 @@
+package com.marketplace.Helpers.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AWSS3Config {
+
+	@Value("${aws.access_key_id}")
+	private String accessKeyId;
+	@Value("${aws.secret_access_key}")
+	private String secretAccessKey;
+	@Value("${aws.s3.region}")
+	private String region;
+
+	@Bean
+	public AmazonS3 getAmazonS3Cient() {
+		final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
+		// Get AmazonS3 client and return the s3Client object.
+		return AmazonS3ClientBuilder
+				.standard()
+				.withRegion(Regions.fromName(region))
+				.withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+				.build();
+	}
+}
