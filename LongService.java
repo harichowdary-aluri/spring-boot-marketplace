9ec57753b8104746a47f23a591ed0b4c33b49fb0
S547051 <aluriharikiran01@gmail.com>
2023-03-03 19:46:29 -0600

    Add Update password
    this method is used ti update password when user want to change the password

---
+++ LongService.java
@@ -0,0 +1,22 @@
+package com.marketplace.Services;
+
+public class LongService {
+
+    @BeforeEach
+    public void init() {
+        User user = userRepository.findByEmail("test@test.com");
+        if (user == null) {
+            user = new User();
+            user.setFirstName("Test");
+            user.setLastName("Test");
+            user.setPassword(passwordEncoder.encode("test"));
+            user.setEmail("test@test.com");
+            user.setEnabled(true);
+            userRepository.save(user);
+        } else {
+            user.setPassword(passwordEncoder.encode("test"));
+            userRepository.save(user);
+        }
+    }
+
+}
