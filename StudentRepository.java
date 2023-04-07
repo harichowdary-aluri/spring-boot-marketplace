30fd214aa4de9843ba3c26aaa816d650fbc36213
S547051 <aluriharikiran01@gmail.com>
2023-04-07 18:20:53 -0500

    Add Student Repository

---
+++ StudentRepository.java
@@ -0,0 +1,16 @@
+package com.marketplace.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marketplace.*;
+import com.marketplace.Entity.Student;
+@Repository
+public interface StudentRepository extends JpaRepository<Student,String>
+{
+
+	Student findByEmail(String email);
+
+}
+
+
+
