a554f29e01ddcb5b386ef55219ed0e31f1422bbc
Kiran Kumar Gundapuneni <98048115+kirangundapuneni@users.noreply.github.com>
2023-04-14 12:10:36 -0500

    Add - Comments in Login file

--- CodeCleanup/Login.java
+++ CodeCleanup/Login.java
@@ -1,10 +1,16 @@
+/**
+ * @Author: Your name
+ * @Date:   2023-02-17 19:36:33
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2023-04-14 12:10:07
+ */
 package com.marketplace.Entity;
 
 import javax.persistence.Entity;
 
 public class Login {
 	
-	
+	// Adding User and password
 	private String username;
 	private String password;
 	
